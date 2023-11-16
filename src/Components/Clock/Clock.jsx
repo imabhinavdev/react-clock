2ed2df499cb08adff9f02a6a43bdc92115ea6919
Abhinav Singh <abhinavas430@gmail.com>
2023-11-16 23:06:02 +0530

    Fix screen

--- src/Components/Clock/Clock.jsx
+++ src/Components/Clock/Clock.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import screenfull from 'screenfull';
 
 const Clock = () => {
     const [currentTime, setCurrentTime] = useState(getCurrentTime());
-    const [isFullScreen, setIsFullScreen] = useState('Full Screen');
     const [isSeconds, setIsSeconds] = useState(false);
 
     useEffect(() => {
@@ -10,23 +10,49 @@ const Clock = () => {
             setCurrentTime(getCurrentTime());
         }, 1000);
 
-        return () => clearInterval(intervalId); // Cleanup the interval on component unmount
+        const handleKeyDown = (event) => {
+            if (screenfull.isFullscreen) {
+                event.preventDefault();
+                if (!(event.ctrlKey && event.key === 'l')) {
+                    event.stopPropagation(); // Stop other key events when in fullscreen
+                }
+            } else {
+                if (event.ctrlKey && event.key === 'l') {
+                    screenfull.exit();
+                }
+
+                if (event.key === 'F11' && screenfull.enabled) {
+                    event.preventDefault(); // Prevent default behavior of F11
+                    if (!screenfull.isFullscreen) {
+                        screenfull.request();
+                    }
+                }
+            }
+        };
+
+        const handleFullScreenChange = () => {
+            if (screenfull.isFullscreen) {
+                document.addEventListener('keydown', handleKeyDown);
+            } else {
+                document.removeEventListener('keydown', handleKeyDown);
+            }
+        };
+
+        screenfull.on('change', handleFullScreenChange);
+
+        return () => {
+            clearInterval(intervalId);
+            document.removeEventListener('keydown', handleKeyDown);
+            screenfull.off('change', handleFullScreenChange);
+        };
     }, []);
 
     function getCurrentTime() {
         const currentDate = new Date();
         let hours = currentDate.getHours();
         const minutes = currentDate.getMinutes();
-        const seconds = currentDate.getSeconds();
         const ampm = hours >= 12 ? 'PM' : 'AM';
         hours = hours % 12 || 12; // Convert to 12-hour format
-
-        // if (isSeconds) {
-        //     return {
-        //         time: `${formatTimeComponent(hours)}:${formatTimeComponent(minutes)}:${formatTimeComponent(seconds)}`,
-        //         ampm: ampm,
-        //     };
-        // }
 
         return {
             time: `${formatTimeComponent(hours)}:${formatTimeComponent(minutes)}`,
@@ -37,17 +63,7 @@ const Clock = () => {
     function formatTimeComponent(timeComponent) {
         return timeComponent < 10 ? `0${timeComponent}` : timeComponent;
     }
-    function fullScreen() {
-        console.log('full screen');
-        if (document.fullscreenElement) {
-            document.exitFullscreen();
-            setIsFullScreen('Full Screen');
-        } else {
-            document.documentElement.requestFullscreen();
-            setIsFullScreen('Exit');
-        }
 
-    }
     function handleSeconds() {
         setIsSeconds(!isSeconds);
     }
@@ -57,8 +73,8 @@ const Clock = () => {
             <div className='flex items-center justify-center w-screen h-screen relative'>
                 <h1 className='text-9xl font-bold'>{currentTime.time}</h1>
                 <span className="text-2xl font-medium">{currentTime.ampm}</span>
-                <button className='rounded-md absolute bottom-10 right-10 border-black border-2 w-32 font-medium' onClick={fullScreen}>{isFullScreen}</button>
-                <button className='rounded-md absolute bottom-10 left-10 border-black border-2 w-36 font-medium' onClick={handleSeconds}>{isSeconds ? "Show Seconds" : "Hide Seconds"}</button>
+                <button className='rounded-md absolute bottom-10 right-10 border-black border-2 w-32 font-medium' onClick={screenfull.toggle}>{screenfull.isFullscreen ? 'Exit Full Screen' : 'Full Screen'}</button>
+                <button className='rounded-md absolute bottom-10 left-10 border-black border-2 w-36 font-medium' onClick={handleSeconds}>{isSeconds ? 'Show Seconds' : 'Hide Seconds'}</button>
             </div>
         </>
     );
